978c610ecc8e5f65e3715336ff1a5155768c6fcb
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-08-01 09:27:42 +0200

    Initial application generated by JHipster-6.2.0

--- src/main/docker/couchbase/Couchbase.Dockerfile
+++ src/main/docker/couchbase/Couchbase.Dockerfile
@@ -1,4 +1,4 @@
-FROM couchbase:6.0.1
+FROM couchbase:6.0.2
 
 COPY couchbase/scripts/configure-node.sh /
 
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'bank-account',
+        loadChildren: () => import('./bank-account/bank-account.module').then(m => m.JhipsterCouchbaseSampleApplicationBankAccountModule)
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
