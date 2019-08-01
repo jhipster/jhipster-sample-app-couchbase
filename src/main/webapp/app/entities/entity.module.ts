a0dc505481c1b5fca0f4d728e95910ad5924e23c
Pascal Grimaud <pascalgrimaud@gmail.com>
2019-08-01 09:28:37 +0200

    Initial application generated by JHipster-6.2.0

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,10 +4,6 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      {
-        path: 'bank-account',
-        loadChildren: () => import('./bank-account/bank-account.module').then(m => m.JhipsterCouchbaseSampleApplicationBankAccountModule)
-      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
