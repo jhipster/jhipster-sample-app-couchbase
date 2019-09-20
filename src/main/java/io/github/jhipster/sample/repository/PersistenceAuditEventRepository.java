5205d3fa5def069d6ab7f381ae0c7cfd7a61b7c6
Julien Dubois <julien.dubois@gmail.com>
2019-09-20 14:53:57 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/repository/PersistenceAuditEventRepository.java
+++ src/main/java/io/github/jhipster/sample/repository/PersistenceAuditEventRepository.java
@@ -4,9 +4,6 @@ import io.github.jhipster.sample.domain.PersistentAuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-
-
-
 import java.time.Instant;
 import java.util.List;
 
@@ -16,10 +13,6 @@ import java.util.List;
 public interface PersistenceAuditEventRepository extends N1qlCouchbaseRepository<PersistentAuditEvent, String> {
 
     List<PersistentAuditEvent> findByPrincipal(String principal);
-
-    List<PersistentAuditEvent> findByAuditEventDateAfter(Instant after);
-
-    List<PersistentAuditEvent> findByPrincipalAndAuditEventDateAfter(String principal, Instant after);
 
     List<PersistentAuditEvent> findByPrincipalAndAuditEventDateAfterAndAuditEventType(String principal, Instant after, String type);
 
---
+++ src/main/webapp/app/entities/bank-account/bank-account-update.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit } from '@angular/core';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IBankAccount, BankAccount } from 'app/shared/model/bank-account.model';
+import { BankAccountService } from './bank-account.service';
+
+@Component({
+  selector: 'jhi-bank-account-update',
+  templateUrl: './bank-account-update.component.html'
+})
+export class BankAccountUpdateComponent implements OnInit {
+  isSaving: boolean;
+
+  editForm = this.fb.group({
+    id: [],
+    name: [null, [Validators.required]],
+    balance: [null, [Validators.required]]
+  });
+
+  constructor(protected bankAccountService: BankAccountService, protected activatedRoute: ActivatedRoute, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.isSaving = false;
+    this.activatedRoute.data.subscribe(({ bankAccount }) => {
+      this.updateForm(bankAccount);
+    });
+  }
+
+  updateForm(bankAccount: IBankAccount) {
+    this.editForm.patchValue({
+      id: bankAccount.id,
+      name: bankAccount.name,
+      balance: bankAccount.balance
+    });
+  }
+
+  previousState() {
+    window.history.back();
+  }
+
+  save() {
+    this.isSaving = true;
+    const bankAccount = this.createFromForm();
+    if (bankAccount.id !== undefined) {
+      this.subscribeToSaveResponse(this.bankAccountService.update(bankAccount));
+    } else {
+      this.subscribeToSaveResponse(this.bankAccountService.create(bankAccount));
+    }
+  }
+
+  private createFromForm(): IBankAccount {
+    return {
+      ...new BankAccount(),
+      id: this.editForm.get(['id']).value,
+      name: this.editForm.get(['name']).value,
+      balance: this.editForm.get(['balance']).value
+    };
+  }
+
+  protected subscribeToSaveResponse(result: Observable<HttpResponse<IBankAccount>>) {
+    result.subscribe(() => this.onSaveSuccess(), () => this.onSaveError());
+  }
+
+  protected onSaveSuccess() {
+    this.isSaving = false;
+    this.previousState();
+  }
+
+  protected onSaveError() {
+    this.isSaving = false;
+  }
+}
