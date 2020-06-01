c1f3be038df387d35c71bce1fb6f55d2c956cbf6
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-06-01 22:38:26 +0200

    automatic project update

---
+++ src/main/java/io/github/jhipster/sample/repository/BankAccountRepository.java
@@ -0,0 +1,13 @@
+package io.github.jhipster.sample.repository;
+
+import io.github.jhipster.sample.domain.BankAccount;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring Data Couchbase repository for the BankAccount entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface BankAccountRepository extends N1qlCouchbaseRepository<BankAccount, String> {
+}
