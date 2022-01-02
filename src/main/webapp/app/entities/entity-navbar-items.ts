d1de1ae721621bf6d90c59276abc5e2190f525d3
Pascal Grimaud <pascalgrimaud@gmail.com>
2022-01-02 11:11:56 +0100

    automatic project update

--- src/main/webapp/app/entities/entity-navbar-items.ts
+++ src/main/webapp/app/entities/entity-navbar-items.ts
@@ -1,7 +1,1 @@
-export const EntityNavbarItems = [
-  {
-    name: 'BankAccount',
-    route: 'bank-account',
-    translationKey: 'global.menu.entities.bankAccount',
-  },
-];
+export const EntityNavbarItems = [];
